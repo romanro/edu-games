03612cfd96dd8685244f617cb2182658fd58938b
Roman Rozanov <antifreez@gmail.com>
2023-10-31 11:11:29 +0200

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'edugames-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'edu-games';
+}
